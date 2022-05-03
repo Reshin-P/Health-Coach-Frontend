b0c80a5bd3c98839f580eeedad667683f5b941dd
Reshin-P <reshinsuresh1@gmail.com>
2022-05-03 21:58:49 +0530

    trainer chat startrd

--- src/components/Trainer/ViewUserProfile.js
+++ src/components/Trainer/ViewUserProfile.js
@@ -1,11 +1,115 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 import './ViewUserProfile.css'
+import { CONNECT_TRAINER, DIET_INSTRUCTION, DIET_PLAN, MESSAGE, VIDEO_CALL, WHATSAPP } from '../../constances/SubscribedWorkout';
+import { Button } from 'react-bootstrap'
 
 const ViewUserProfile = () => {
     return (
-        <div>
-            fdfdf
-        </div>
+        <Row className='row-profile' >
+            <Col sm={12} lg={7} md={12} xl={7} className='Col1' >
+                <div className='details'>
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Name </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>Reshi</p>
+                        </div>
+                    </div>
+
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Email </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>Reshfddfdfdfi</p>
+                        </div>
+                    </div>
+
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Mob NO </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>Reshi</p>
+                        </div>
+                    </div>
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Age </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>55</p>
+                        </div>
+                    </div>
+
+
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Height </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>44</p>
+                        </div>
+                    </div>
+
+
+
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Weight </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>Reshi</p>
+                        </div>
+                    </div>
+
+                    <div className='text'>
+                        <div className='key'>
+                            <p>Health Condition </p>
+                        </div>
+                        <div className='colon'>
+                            :
+                        </div>
+                        <div className='value'>
+                            <p>Reshi</p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </Col>
+            <Col sm={12} lg={4} md={12} xl={4} className='Col1 border shadow ' >
+                <div className="connect ">
+                    <h2>{CONNECT_TRAINER}</h2>
+                    <Button className="mt-5 btn1" variant="contained">{VIDEO_CALL}</Button>
+                    <Button className="mt-5 btn2" variant="contained">{MESSAGE}</Button>
+                    <Button className="mt-5 btn3" variant="contained">{WHATSAPP}</Button>
+                </div>
+            </Col>
+
+        </Row>
     )
 }
 
--- src/screens/Trainer/ViewUserScreen.js
+++ src/screens/Trainer/ViewUserScreen.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import HeaderTrainer from '../../components/Trainer/HeaderTrainer'
 import ViewUserPhoto from '../../components/Trainer/ViewUserPhoto'
 import ViewUserProfile from '../../components/Trainer/ViewUserProfile'
+import SingleworkoutUserScreen from '../../components/Trainer/SingleworkoutUserScreen'
+import { SINGLE_USER } from '../../util/Api'
+import { useParams } from 'react-router-dom'
+import axios from '../../util/axios'
+import { height } from '@mui/system'
 
 const ViewUserScreen = () => {
+    const params = useParams()
+
+    const singleUser = async () => {
+        console.log("reache api call");
+        try {
+            const { data } = axios.get(SINGLE_USER, params.id)
+        } catch (error) {
+
+        }
+
+    }
+    useEffect(() => {
+        singleUser()
+    }, [])
+
     return (
         <>
             <HeaderTrainer />
-            <Container style={{ height: "800px" }} >
+            <Container style={{ minHeight: "800px", height: "auto" }} className='border shadow' >
                 <ViewUserPhoto />
                 <ViewUserProfile />
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <div style={{ width: "93%" }}>
+
+
+                        <SingleworkoutUserScreen />
+                        <SingleworkoutUserScreen />
+
+                    </div>
+                </div>
 
             </Container>
         </>
